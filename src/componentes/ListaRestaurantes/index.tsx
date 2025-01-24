53c06dca39de2406c5a0c900a957d6aeba47d26e
Mateus Bernart <mateusbernart14@gmail.com>
2025-01-24 09:51:56 -0300

    add search by filter functionality

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -4,7 +4,14 @@ import style from "./ListaRestaurantes.module.scss";
 import Restaurante from "./Restaurante";
 import axios, { AxiosRequestConfig } from "axios";
 import IPaginacao from "../../interfaces/IPaginacao";
-import { Button, Input } from "@mui/material";
+import {
+  Box,
+  Button,
+  Input,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
 
 interface IParametrosBusca {
   ordering?: string;
@@ -17,6 +24,8 @@ const ListaRestaurantes = () => {
   const [paginaAnterior, setPaginaAnterior] = useState("");
 
   const [busca, setBusca] = useState("");
+
+  const [ordenacao, setOrdenacao] = useState("");
 
   const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
     axios
@@ -31,16 +40,18 @@ const ListaRestaurantes = () => {
       });
   };
 
-  const buscar = (evento: React.FormEvent<HTMLFormElement>) => {
-    evento.preventDefault();
+  useEffect(() => {
     const opcoes = {
       params: {} as IParametrosBusca,
     };
     if (busca) {
       opcoes.params.search = busca;
     }
+    if (ordenacao) {
+      opcoes.params.ordering = ordenacao;
+    }
     carregarDados("http://localhost:8000/api/v1/restaurantes/", opcoes);
-  };
+  }, [ordenacao, busca]);
 
   useEffect(() => {
     //obter restaurantes
@@ -52,13 +63,27 @@ const ListaRestaurantes = () => {
       <h1>
         Os restaurantes mais <em>bacanas</em>!
       </h1>
-      <form onSubmit={buscar}>
-        <Input
-          type="text"
-          value={busca}
-          onChange={(evento) => setBusca(evento.target.value)}
-        ></Input>
-        <Button type="submit">BUSCAR</Button>
+      <form onSubmit={(evento) => evento.preventDefault()}>
+        <Box>
+          <InputLabel>Ordenação</InputLabel>
+          <Select
+            labelId="select-ordenacao-label"
+            id="select-ordenacao"
+            value={ordenacao}
+            onChange={(evento) => setOrdenacao(evento.target.value)}
+          >
+            <MenuItem value="">Selecione um campo</MenuItem>
+            <MenuItem value={"id"}>Id</MenuItem>
+            <MenuItem value={"nome"}>Nome</MenuItem>
+          </Select>
+          <Input
+            sx={{ marginLeft: 2 }}
+            type="text"
+            value={busca}
+            onChange={(evento) => setBusca(evento.target.value)}
+          ></Input>
+          <Button type="submit">BUSCAR</Button>
+        </Box>
       </form>
       {restaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
@@ -80,5 +105,4 @@ const ListaRestaurantes = () => {
     </section>
   );
 };
-
 export default ListaRestaurantes;
